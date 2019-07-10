c138d577b6d517d4ce6f6dcc21112297919ab558
sanjayaajo <50911421+sanjayaajo@users.noreply.github.com>
2019-07-11 01:06:38 +0700

    Add files via upload

---
+++ sanjayapulsa.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Lumayan Buat Pamer Ke Kawan
+#Recode Pun Bodoh Amat Yg Penting Asyik
+#Tools Ini Cuman Buat Iseng² doang
+
+figlet Pulsa Free |lolcat
+
+echo "\033[0;31m]
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(  )/    -----
+   //////   * \/ *   ---
+  //// / // :    : ---
+ // /   /  /*    *--
+//          //..\\
+===========UU====UU==============
++          *//||\\*             +
++            ****               +
++ coded by : SanjayaDtm         +
++ Team : CYBER HACKER TEAM CLAY +
++ kontak me : 6283197998878     +
++                               +
++  ^...^                 ^...^  +
++ <_* *_> TransferPulsa <_* *_> +
++   \_/                   \_/   +
+================================="
+
+echo "\033[0;32m]Masukan Nomer Anda: "
+read anda
+echo "\033[0;32m]Masukan Nomer Korban: "
+read korban
+echo "\033[0;32m]Jumblah Pulsa : "
+read pulsa
+echo
+echo "Wait"
+echo
+echo "$korban, Sukses Ngirim Pulsa $pulsa"
+echo "Done BosQ"
